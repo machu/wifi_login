e2305d54896b5be6dd4a718dce37eec87b35a364
MATSUOKA Kohei <kohei@machu.jp>
2012-09-25 09:47:25 +0900

    update spec

--- spec/wifi_login/providers/docomo_spec.rb
+++ spec/wifi_login/providers/docomo_spec.rb
@@ -3,15 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + './../../spec_helper')
 require 'wifi_login/providers/docomo'
 
 describe WifiLogin::Providers::Docomo do
-  describe '#login?' do
-    before do
-      ssid = 'docomo'
-      @provider = WifiLogin.providers[ssid]
-    end
+  before do
+    @provider = WifiLogin::Providers::Docomo.new
+  end
 
+  describe '#login' do
     context "when successful" do
-      let(:credential) { { :id => 'test', :pass => 'password' } }
-      subject { @provider.login(credential) }
+      let(:id) { 'id' }
+      let(:password) { 'password' }
+      subject { @provider.login(id, password) }
 
       it "ログイン成功を返す" do
         subject.should be true
